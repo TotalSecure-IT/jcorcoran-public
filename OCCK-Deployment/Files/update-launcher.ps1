dd13b66753c61e697fce568ea982354aa6393bd0
jc0rc0ran <justin@tspro.com>
2025-03-19 16:06:06 -0500

    finalizing

--- OCCK-Deployment/Files/update-launcher.ps1
+++ OCCK-Deployment/Files/update-launcher.ps1
@@ -1,11 +1,11 @@
 # Define GitHub raw URLs (replace with your actual GitHub repo URLs)
-$GITHUB_RAW_URL_BAT = "https://raw.githubusercontent.com/TotalSecure-IT/jcorcoran-public/refs/heads/main/OCCK-Deployment/Files/occk-deploy-test.bat"
-$GITHUB_RAW_URL_PS1 = "https://raw.githubusercontent.com/TotalSecure-IT/jcorcoran-public/refs/heads/main/OCCK-Deployment/Files/occk-deploy-test.ps1"
+$GITHUB_RAW_URL_BAT = "https://raw.githubusercontent.com/TotalSecure-IT/jcorcoran-public/refs/heads/main/OCCK-Deployment/Files/Occk-Deploy.bat"
+$GITHUB_RAW_URL_PS1 = "https://raw.githubusercontent.com/TotalSecure-IT/jcorcoran-public/refs/heads/main/OCCK-Deployment/Files/Occk-Deploy.ps1"
 
 # Define local file paths
 $SCRIPT_FOLDER = "$PSScriptRoot"
-$LOCAL_BAT = "$SCRIPT_FOLDER\Occk-Deploy-Test.bat"
-$LOCAL_PS1 = "$SCRIPT_FOLDER\Occk-Deploy-Test.ps1"
+$LOCAL_BAT = "$SCRIPT_FOLDER\Occk-Deploy.bat"
+$LOCAL_PS1 = "$SCRIPT_FOLDER\Occk-Deploy.ps1"
 
 # Function to download and replace files
 Function Update-File {
@@ -30,5 +30,5 @@ Update-File -Url $GITHUB_RAW_URL_BAT -Destination $LOCAL_BAT
 Update-File -Url $GITHUB_RAW_URL_PS1 -Destination $LOCAL_PS1
 
 # Launch Occk-Deploy.bat
-Write-Host "Launching Occk-Deploy-Test.bat..."
+Write-Host "Launching Occk-Deploy.bat..."
 Start-Process -FilePath $LOCAL_BAT -NoNewWindow -Wait

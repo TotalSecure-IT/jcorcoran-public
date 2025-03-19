93602b8b808e5772894f1ae8244f78845c6488dc
jc0rc0ran <125287414+jc0rc0ran@users.noreply.github.com>
2025-03-19 16:13:38 -0500

    finalizing

--- OCCK-Deployment/Files/Occk-Deploy.bat
+++ OCCK-Deployment/Files/Occk-Deploy.bat
@@ -1,16 +1,16 @@
 @echo off
-REM
+REM Check if PowerShell 7 is already installed
 if exist "C:\Program Files\PowerShell\7\pwsh.exe" (
     echo PowerShell 7 detected. Launching script directly...
     "C:\Program Files\PowerShell\7\pwsh.exe" -ExecutionPolicy Bypass -File "C:\OCCK-Deployment\Files\Occk-Deploy.ps1"
     exit /b
 )
 
-REM
+REM If PS7 is not installed, run the script with the existing PowerShell
 echo PowerShell 7 not detected. Running script with PowerShell 5...
 powershell.exe -ExecutionPolicy Bypass -File "C:\OCCK-Deployment\Files\Occk-Deploy.ps1"
 
-REM
+REM Wait for PowerShell 7 installation to complete
 :WaitForPS7
 if exist "C:\Program Files\PowerShell\7\pwsh.exe" goto Relaunch
 timeout /t 1 >nul

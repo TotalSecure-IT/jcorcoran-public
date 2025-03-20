ac49acc29e8288f8d0f70e718e7571b91b587252
jc0rc0ran <125287414+jc0rc0ran@users.noreply.github.com>
2025-03-20 17:03:50 -0500

    added acronis

--- CCA-Deployment/Files/CCA-Deploy.ps1
+++ CCA-Deployment/Files/CCA-Deploy.ps1
@@ -1,5 +1,5 @@
 # Start logging stuff to a file
-$logFile = Join-Path $PSScriptRoot "output_log.txt"
+$logFile = Join-Path $PSScriptRoot "deployment_log.txt"
 Start-Transcript -Path $logFile -Append
 
 # Function to read our INI file into a hashtable

3ca93f1a67709e6ea44fb93d8a38c7aacb645899
jc0rc0ran <125287414+jc0rc0ran@users.noreply.github.com>
2025-03-31 13:37:23 -0500

    Update CCA-Deploy.ps1

--- CCA-Deployment/Files/CCA-Deploy.ps1
+++ CCA-Deployment/Files/CCA-Deploy.ps1
@@ -131,7 +131,7 @@ if (-not $inPS7) {
         }
         Write-Host "Updating winget..." -ForegroundColor Cyan
         Write-Log "Updating winget..."
-        winget upgrade winget --silent
+        winget upgrade winget --silent --nowarn --verbose --force --disable-interactivity
         if ($LASTEXITCODE -eq 0) {
             Write-Host "Winget update installed successfully." -ForegroundColor Green
             Write-Log "Winget update installed successfully"

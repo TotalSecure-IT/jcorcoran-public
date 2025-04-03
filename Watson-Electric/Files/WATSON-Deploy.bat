a493bcc32f758937b70ebc37bb7493f82a94c314
jc0rc0ran <justin@tspro.com>
2025-04-03 15:49:54 -0500

    update

--- Watson-Electric/Files/WATSON-Deploy.bat
+++ Watson-Electric/Files/WATSON-Deploy.bat
@@ -2,13 +2,21 @@
 REM Check if PowerShell 7 is already installed
 if exist "C:\Program Files\PowerShell\7\pwsh.exe" (
     echo PowerShell 7 detected. Launching script directly...
+<<<<<<< HEAD
     "C:\Program Files\PowerShell\7\pwsh.exe" -ExecutionPolicy Bypass -File "C:\WATSON-Deployment\Files\CCA-Deploy.ps1"
+=======
+    "C:\Program Files\PowerShell\7\pwsh.exe" -ExecutionPolicy Bypass -File "C:\CCA-Deployment\Files\CCA-Deploy.ps1"
+>>>>>>> 04c8b55b83cc4c2cf2992a0dc54925eb843b8dc4
     exit /b
 )
 
 REM If PS7 is not installed, run the script with the existing PowerShell
 echo PowerShell 7 not detected. Running script with PowerShell 5...
+<<<<<<< HEAD
 powershell.exe -ExecutionPolicy Bypass -File "C:\WATSON-Deployment\Files\CCA-Deploy.ps1"
+=======
+powershell.exe -ExecutionPolicy Bypass -File "C:\CCA-Deployment\Files\CCA-Deploy.ps1"
+>>>>>>> 04c8b55b83cc4c2cf2992a0dc54925eb843b8dc4
 
 REM Wait for PowerShell 7 installation to complete
 :WaitForPS7
@@ -19,4 +27,8 @@ goto WaitForPS7
 :Relaunch
 echo PowerShell 7 is now installed.
 echo Relaunching script using PowerShell 7...
+<<<<<<< HEAD
 "C:\Program Files\PowerShell\7\pwsh.exe" -ExecutionPolicy Bypass -File "C:\WATSON-Deployment\Files\CCA-Deploy.ps1"
+=======
+"C:\Program Files\PowerShell\7\pwsh.exe" -ExecutionPolicy Bypass -File "C:\CCA-Deployment\Files\CCA-Deploy.ps1"
+>>>>>>> 04c8b55b83cc4c2cf2992a0dc54925eb843b8dc4
--- Watson-Electric/Files/deploy.bat
+++ Watson-Electric/Files/deploy.bat
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 powershell.exe -ExecutionPolicy Bypass -File "C:\WATSON-Deployment\Files\update-launcher.ps1"
+=======
+powershell.exe -ExecutionPolicy Bypass -File "C:\CCA-Deployment\Files\update-launcher.ps1"
+>>>>>>> 04c8b55b83cc4c2cf2992a0dc54925eb843b8dc4

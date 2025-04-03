f380e15c6168507903b6841b50be9ab3e303a475
jc0rc0ran <justin@tspro.com>
2025-04-03 15:54:29 -0500

    Update deploy.bat

--- Watson-Electric/deploy.bat
+++ Watson-Electric/deploy.bat
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 powershell.exe -ExecutionPolicy Bypass -File "C:\WATSON-Deployment\Files\update-launcher.ps1"
-=======
-powershell.exe -ExecutionPolicy Bypass -File "C:\CCA-Deployment\Files\update-launcher.ps1"
->>>>>>> 04c8b55b83cc4c2cf2992a0dc54925eb843b8dc4

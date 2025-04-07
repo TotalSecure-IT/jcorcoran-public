f0fde8594eb1f810de4fec3b711ff0437a1673b1
jc0rc0ran <justin@tspro.com>
2025-04-07 09:11:48 -0500

    Update main.ps1

--- Poly.PKit/main.ps1
+++ Poly.PKit/main.ps1
@@ -1,6 +1,6 @@
 # Check for mode flags passed as arguments
 if ($args -contains '--online-mode') {
-    Write-Host "Running in ONLINE mode"
+    Write-Host "Running in SUPER FUCKIN AWESOME ONLINE mode"
 } elseif ($args -contains '--cached-mode') {
     Write-Host "Running in CACHED mode"
 } else {
